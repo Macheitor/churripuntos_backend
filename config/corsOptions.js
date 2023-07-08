46b4c65574019bfc8183a97f2749e82a9a70d211
Victor Machado <victor.machado.perez@gmail.com>
2023-07-09 01:57:11 +0200

    Include React+Vite default server port

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -5,6 +5,7 @@ if (process.env.NODE_ENV !== 'production') {
     whitelist.push(
         'http://127.0.0.1:5500',
         'http://localhost:8080',
+        'http://localhost:5173',
         'localhost:8080')
 }
 
