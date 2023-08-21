be61d198a394d7862497feb819f3510b8c57f9ef
Victor Machado <victor.machado.perez@gmail.com>
2023-08-21 14:53:55 +0200

    Verify session only needs the jwt

--- controller/verifySessionController.js
+++ controller/verifySessionController.js
@@ -1,24 +1,16 @@
 if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config();
 }
-const {errLogger} = require('../middlewares/logger');
 
-const jwt = require('jsonwebtoken')
+const {errLogger} = require('../middlewares/logger');
 
 module.exports = async (req, res) => {
 
     try {
-        // Comparing the decoded jwt values with given request values
-        if (req.body.username === req.username &&
-            req.body.userId === req._id) {
-
-                res.status(200).send({
-                    status: "success",
-                    message: "Session is valid"
-                });
-            } else {
-                res.status(403).send({status: 'fail', message: 'Session is not valid'});
-            }
+        res.status(200).send({
+            status: "success",
+            message: "Valid accesToken"
+        });
 
      } catch(err) {
         const error = { status: 'error', message: `${err.name}: ${err.message}` }; 
